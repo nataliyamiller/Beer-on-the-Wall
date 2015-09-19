a30ec38a9e8c1624a3ed8b5587e715e501854261
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-18 19:27:27 -0700

    All specs are passing

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,35 @@ var beerOnTheWall = function(numberOfBottles) {
   }
   return bottlesArray;
 }
+
+var oneBeerLeft = function() {
+  return ("Only 1 bottle of beer on the wall, only 1 bottle of beer." +
+  " Take this last one down and pass it around, no more bottles of beer on the wall, no more bottles of beer." +
+  "  Go to the store and buy some more, currently no bottles of beer on the wall.");
+}
+
+
+$(document).ready(function() {
+  $("form#bottles").submit(function(event) {
+    var userInput = parseInt($("input#user-input").val());
+    var bottles = hasBeer(userInput);
+    var hasBottles = beerOnTheWall(userInput);
+    var oneBottleLeft = oneBeerLeft();
+
+    if(!bottles) {
+      $("#result-1").show();
+      $("#result-2").hide();
+    } else {
+      $("#result-1").hide();
+      $("#result-2").slideDown();
+      $("ul").empty();
+      var number = hasBottles.length;
+      for (var i = 0; i < number - 1 ; i++) {
+      $("ul").append("<li>" + hasBottles[i] + "bottles of beer on the wall, " + hasBottles[i] +
+      "bottles of beer. Take one down and pass it around, " + hasBottles[i+1] + "bottles of beer on the wall.</li>");
+    }
+    $("ul").append("<li>" + oneBottleLeft + "</li>");
+    }
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('hasBeer', function() {
-  it("is false if there is no more beer on the wall", function() {
+  it("is false if there is no beer on the wall", function() {
     expect(hasBeer(0)).to.equal(false);
   });
 
@@ -9,7 +9,17 @@ describe('hasBeer', function() {
 });
 
 describe('beerOnTheWall', function() {
-  it("returns array of bottles in descending order", function() {
+  it("turns user's number of bottles into an array", function() {
+    expect(beerOnTheWall(5)).to.be.instanceOf(Array);
+  });
+
+  it("returns array of numbers in descending order", function() {
     expect(beerOnTheWall(5)).to.eql([5, 4, 3, 2, 1]);
   });
 });
+
+describe('oneBeerLeft', function() {
+  it("returns a warning message that no more beer is left after passing around the last bottle", function() {
+    expect(oneBeerLeft()).contains("Take this last one down and pass it around, no more bottles of beer on the wall");
+  })
+})
